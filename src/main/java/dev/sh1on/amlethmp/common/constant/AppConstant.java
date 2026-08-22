55ce4dc9c9039d2650bfc2d43d9322a9da72fa8b
Dandelion <quochung200337@gmail.com>
2026-08-22 02:17:43 +0000

    Dump SP version to 4.0.8

--- src/main/java/dev/sh1on/amlethmp/common/constant/AppConstant.java
+++ src/main/java/dev/sh1on/amlethmp/common/constant/AppConstant.java
@@ -32,9 +32,4 @@ public final class AppConstant {
      * @see <a href="https://man.archlinux.org/man/xdg-open.1.en">xdg-open (Arch Linux)</a>
      */
     public static final String OPEN_LINUX = "/usr/bin/xdg-open";
-
-    /**
-     * Tên System property đánh dấu Swagger UI đã được tự mở trên trình duyệt trong phiên JVM hiện tại.
-     */
-    public static final String SWAGGER_UI_OPENED_PROPERTY = "amlethmp.devtools.swagger-ui-opened";
 }
